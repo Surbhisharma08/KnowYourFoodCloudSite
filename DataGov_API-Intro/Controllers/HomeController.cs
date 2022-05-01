fbcfd8b7933f76320f5f2bae66883917308ab468
Surbhi <ssharma281@usf.edu>
2022-05-01 16:14:49 -0400

    Hi, I created view part for know your food API. Data is not populating. I made the changes for the required output but data variable is not populating as it it routing through the API. Model Changes-2

--- DataGov_API-Intro/Controllers/HomeController.cs
+++ DataGov_API-Intro/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DataGov_API_Intro.Controllers
         //static string BASE_URL = "https://developer.nps.gov/api/v1";
         static string BASE_URL = "https://api.nal.usda.gov/fdc/v1";
         static string API_KEY = "yAdUGQQabSyuMX0CGur5SuEhS5Ud6xszLAWDMsr1"; //Add your API key here inside ""
-        static string query = "cheddar cheese";
+        static string query = "banana";
         //static string BASE_URL = "https://data.cdc.gov/api/views/hk9y-quqm/rows.json";
         // Obtaining the API key is easy. The same key should be usable across the entire
         // data.gov developer network, i.e. all data sources on data.gov.
@@ -80,23 +80,203 @@ namespace DataGov_API_Intro.Controllers
         }
         public ActionResult food()
         {
-            
-            return View();
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Add("query", query);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            // accept data-type in json format
+
+            //string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string NATIONAL_FOOD_API_PATH = BASE_URL + "/foods/search?limit=20";
+            string foodsData = "";
+
+            Rootobject foods = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_FOOD_API_PATH);
+            //httpClient.BaseAddress = new Uri(BASE_URL);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_FOOD_API_PATH)
+                                                        .GetAwaiter().GetResult();
+                //HttpResponseMessage response = httpClient.GetAsync(BASE_URL)
+                //                                        .GetAwaiter().GetResult();
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    foodsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!foodsData.Equals(""))
+                {
+                    //JsonConvert is part of the NewtonSoft.Json Nuget package
+                    foods = JsonConvert.DeserializeObject<Rootobject>(foodsData);
+                }
+
+                //dbContext.Parks.Add(parks);
+                //await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(foods);
         }
         public ActionResult Recipie()
         {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Add("query", query);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            // accept data-type in json format
 
-            return View();
+            //string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string NATIONAL_FOOD_API_PATH = BASE_URL + "/foods/search?limit=20";
+            string foodsData = "";
+
+            Rootobject foods = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_FOOD_API_PATH);
+            //httpClient.BaseAddress = new Uri(BASE_URL);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_FOOD_API_PATH)
+                                                        .GetAwaiter().GetResult();
+                //HttpResponseMessage response = httpClient.GetAsync(BASE_URL)
+                //                                        .GetAwaiter().GetResult();
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    foodsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!foodsData.Equals(""))
+                {
+                    //JsonConvert is part of the NewtonSoft.Json Nuget package
+                    foods = JsonConvert.DeserializeObject<Rootobject>(foodsData);
+                }
+
+                //dbContext.Parks.Add(parks);
+                //await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(foods);
         }
         public ActionResult AboutUs()
         {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Add("query", query);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            // accept data-type in json format
 
-            return View();
+            //string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string NATIONAL_FOOD_API_PATH = BASE_URL + "/foods/search?limit=20";
+            string foodsData = "";
+
+            Rootobject foods = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_FOOD_API_PATH);
+            //httpClient.BaseAddress = new Uri(BASE_URL);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_FOOD_API_PATH)
+                                                        .GetAwaiter().GetResult();
+                //HttpResponseMessage response = httpClient.GetAsync(BASE_URL)
+                //                                        .GetAwaiter().GetResult();
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    foodsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!foodsData.Equals(""))
+                {
+                    //JsonConvert is part of the NewtonSoft.Json Nuget package
+                    foods = JsonConvert.DeserializeObject<Rootobject>(foodsData);
+                }
+
+                //dbContext.Parks.Add(parks);
+                //await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(foods);
         }
         public ActionResult Component()
         {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Add("query", query);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            // accept data-type in json format
 
-            return View();
+            //string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string NATIONAL_FOOD_API_PATH = BASE_URL + "/foods/search?limit=20";
+            string foodsData = "";
+
+            Rootobject foods = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_FOOD_API_PATH);
+            //httpClient.BaseAddress = new Uri(BASE_URL);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_FOOD_API_PATH)
+                                                        .GetAwaiter().GetResult();
+                //HttpResponseMessage response = httpClient.GetAsync(BASE_URL)
+                //                                        .GetAwaiter().GetResult();
+
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    foodsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!foodsData.Equals(""))
+                {
+                    //JsonConvert is part of the NewtonSoft.Json Nuget package
+                    foods = JsonConvert.DeserializeObject<Rootobject>(foodsData);
+                }
+
+                //dbContext.Parks.Add(parks);
+                //await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(foods);
         }
     }
 }
